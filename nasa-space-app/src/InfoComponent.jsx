cd0759f18f6d026fe285ff02feb5700d4efb3b0a
Tanner Stuhr <Tannerjstuhr@gmail.com>
2023-10-07 16:27:00 -0700

    more changes to accodion
    small changes to accordion in infocomponent, need to fill in info

--- nasa-space-app/src/InfoComponent.jsx
+++ nasa-space-app/src/InfoComponent.jsx
@@ -5,35 +5,104 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 const InfoComponent = () => {
     return (
         <div className="accordion" id="accordionExample">
-            {Array.from({ length: 3 }).map((_, index) => (
-                <div className="accordion-item" key={index}>
-                    <h2 className="accordion-header" id={`heading${index}`}>
-                        <button
-                            className={`accordion-button${index === 0 ? '' : ' collapsed'}`}
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target={`#collapse${index}`}
-                            aria-expanded={index === 0}
-                            aria-controls={`collapse${index}`}
-                        >
-                            Collapsible Group Item #{index + 1}
-                        </button>
-                    </h2>
-                    <div
-                        id={`collapse${index}`}
-                        className={`accordion-collapse collapse${index === 0 ? ' show' : ''}`}
-                        aria-labelledby={`heading${index}`}
-                        data-bs-parent="#accordionExample"
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="headingOne">
+                    <button
+                        className="accordion-button"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne"
+                        aria-expanded="true"
+                        aria-controls="collapseOne"
                     >
-                        <div className="accordion-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid...
-                        </div>
+                        Collapsible Group Item #1
+                    </button>
+                </h2>
+                <div
+                    id="collapseOne"
+                    className="accordion-collapse collapse show"
+                    aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample"
+                >
+                    <div className="accordion-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
                     </div>
                 </div>
-            ))}
+            </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="headingTwo">
+                    <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseTwo"
+                        aria-expanded="false"
+                        aria-controls="collapseTwo"
+                    >
+                        Collapsible Group Item #2
+                    </button>
+                </h2>
+                <div
+                    id="collapseTwo"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingTwo"
+                    data-bs-parent="#accordionExample"
+                >
+                    <div className="accordion-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                    </div>
+                </div>
+            </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="headingThree">
+                    <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseThree"
+                        aria-expanded="false"
+                        aria-controls="collapseThree"
+                    >
+                        Collapsible Group Item #3
+                    </button>
+                </h2>
+                <div
+                    id="collapseThree"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingThree"
+                    data-bs-parent="#accordionExample"
+                >
+                    <div className="accordion-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                    </div>
+                </div>
+            </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header" id="headingFour">
+                    <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseFour"
+                        aria-expanded="false"
+                        aria-controls="collapseFour"
+                    >
+                        Collapsible Group Item #4
+                    </button>
+                </h2>
+                <div
+                    id="collapseFour"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingFour"
+                    data-bs-parent="#accordionExample"
+                >
+                    <div className="accordion-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default InfoComponent;
-
