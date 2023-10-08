03785224c58e64421ca5da65d3eb5aa6ee365be3
ttan815 <tonytan815@gmail.com>
2023-10-08 10:57:48 -0700

    Update ButtonComponent.jsx

--- nasa-space-app/src/ButtonComponent.jsx
+++ nasa-space-app/src/ButtonComponent.jsx
@@ -1,36 +1,15 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 
-const ButtonComponent = () => {
-    const [locationData, setLocationData] = useState(null);
-    const [error, setError] = useState(null);
-    const [showButton, setShowButton] = useState(true);
-    const [averageData, setAverageData] = useState(null);
-
-    const resetStates = () => {
-        setLocationData(null);
-        setError(null);
-        setShowButton(true);
-        setAverageData(null);
-    };
-
+const ButtonComponent = ({ setLocationData, setShowButton, setError, showButton, resetStates  }) => {
     const fetchData = async () => {
         try {
-            const ipApiResponse = await axios.get('http://ip-api.com/json/');
-            if (ipApiResponse.status !== 200) {
-                throw new Error('Network response was not ok');
+            const response = await fetch('http://ip-api.com/json/');
+            if (!response.ok) {
+                throw new Error('Network response was not ok' + response.statusText);
             }
-            const data = ipApiResponse.data;
+            const data = await response.json();
             setLocationData(data);
             setShowButton(false);
-
-            // Make a request to your Flask API to get the average data
-            const flaskApiResponse = await axios.get(`http://127.0.0.1:5000/api/${data.lon}/${data.lat}`);
-            if (flaskApiResponse.status !== 200) {
-                throw new Error('Flask server response was not ok');
-            }
-            const flaskData = flaskApiResponse.data;
-            setAverageData(flaskData.average);
         } catch (error) {
             setError(error.toString());
         }
@@ -46,25 +25,6 @@ const ButtonComponent = () => {
                 <button className="btn btn-secondary" onClick={resetStates}>
                     Reset
                 </button>
-            )}
-            {locationData && (
-                <div>
-                    <h2>Location Data</h2>
-                    <p>Latitude: {locationData.lat}</p>
-                    <p>Longitude: {locationData.lon}</p>
-                </div>
-            )}
-            {averageData !== null && (
-                <div>
-                    <h2>NDVI Value</h2>
-                    <p>Average: {averageData}</p>
-                </div>
-            )}
-            {error && (
-                <div>
-                    <h2>Error</h2>
-                    <p>{error}</p>
-                </div>
             )}
         </div>
     );
